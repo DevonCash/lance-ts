9cbb45fdd47bd4e7ef8d77977e15f5a55e076c8b
Devon Cash <devon.p.cash@gmail.com>
2021-01-30 11:51:08 -0800

    Updated deprecated rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
-import pjson from 'rollup-plugin-json';
-import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
+import pjson from '@rollup/plugin-json';
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 
 const serverPlugins = [resolve(), pjson(), commonjs({ include: 'node_modules/**' })];
 const clientPlugins = [resolve({ browser: true, preferBuiltins: false }), pjson(), commonjs({ include: 'node_modules/**' })];
@@ -12,7 +12,7 @@ export default [
         input: 'src/package/serverExports.js',
         external: ['fs', 'bufferutil', 'utf-8-validate'],
         output: { file: 'dist/server/lance-gg.js', format: 'cjs', name: 'Server', sourcemap: true },
-        plugins: [... serverPlugins, babel({ exclude: '**/node_modules/**' })]
+        plugins: [...serverPlugins, babel({ exclude: '**/node_modules/**' })]
     },
     {
         // server build non-transpiled with es2015 modules
@@ -25,7 +25,7 @@ export default [
         // client build transpiled
         input: 'src/package/clientExports.js',
         output: { file: 'dist/client/lance-gg.js', format: 'umd', name: 'Client', sourcemap: true },
-        plugins: [... clientPlugins, babel({ exclude: '**/node_modules/**' })]
+        plugins: [...clientPlugins, babel({ exclude: '**/node_modules/**' })]
     },
     {
         // client build non-transpiled with es2015 modules
